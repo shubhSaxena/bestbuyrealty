a5246098eb85380e4f0a5a7f109065e972e3312a
shubhSaxena <shubhamsaxena04@gmail.com>
2018-12-02 03:20:16 +0530

    form submission

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require('express');
 const app = express();
 const dirPath = require('path');
 const pg = require('pg');
+const bodyParser = require("body-parser");
+
+var pgp = require('pg-promise')
+var db = pgp(process.env.DATABASE_URL)
+
 pg.defaults.ssl = true;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // app.set('views', dirPath.join(__dirname, 'views'));
 // app.engine('html', require('ejs').renderFile);
@@ -19,9 +27,40 @@ app.listen(port, (err) => { console.log('connected!!') });
 
 // index.html
 app.get('/', function (req, res) {
-  res.render('home')
+  res.render('home');
 })
 
-// app.post('/contact', function (req, res){
+app.post('/contact', function (req, res) {
+  console.log("request body")
+  console.log(req.body)
+  var first_name = req.body.first_name;
+  var last_name = req.body.last_name;
+  var phone = req.body.phone;
+  var email = req.body.email;
+  var message = req.body.message;
 
-// })
+  console.log("first name = " + first_name);
+  console.log("last name = " + last_name);
+  console.log("phone = " + phone);
+  console.log("email = " + email);
+  console.log("message = " + message);
+  var data = insertData(req.body);
+  console.log("data -- ", data)
+  if (data) {
+    res.status(200).end("ok");
+  } else {
+    res.status(200).end("error");
+  }
+})
+
+function insertData(data) {
+  db.one('INSERT INTO contact_form(first_name, last_name, phone, email, message) VALUES($1, $2, $3, $4, $5) RETURNING id', [data.first_name, data.last_name, data.phone, data.email, data.message])
+    .then(data => {
+      console.log(data.id); // print new user id;
+      return true;
+    })
+    .catch(error => {
+      console.log('ERROR:', error); // print error;
+      return false;
+    });
+}
---
+++ public/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  var first_name, last_name, phone, email, message;
+  $("#contact_form").submit(function () {
+    alert("Handler for .submit() called.");
+    event.preventDefault();
+    console.log("click")
+    first_name = $("#first_name").val();
+    last_name = $("#last_name").val();
+    phone = $("#phone").val();
+    email = $("#email").val();
+    message = $("#message").val();
+    $.post("/contact", { first_name: first_name, last_name: last_name, phone: phone, email: email, message: message },
+      function (data, status) {
+        if (data === 'ok' && status == "success") {
+          console.log("ok success");
+          $("#success-popup").addClass('show');
+          setTimeout(function () { $("#success-popup").addClass('hide') }, 1000)
+        }
+      });
+  });
+
+});
